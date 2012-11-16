370fb1e001bb1fb7bb3a3e2e8c7f57ff9d1caccb
myobie <nathan@myobie.com>
2012-11-16 15:04:32 -0800

    Trying docs

--- lib/rep.rb
+++ lib/rep.rb
@@ -1,15 +1,39 @@
 # **Rep** is a small module to endow any class to make json quickly. It solves four problems:
 #
-# * Enumerating top level keys for a json structure
-# * Providing a convention for the value of those keys
-# * Defining attr_accessor's that are prefilled from an options hash given to #initialize
-# * Sharing instances to help GC
+# 1. Enumerating top level keys for a json structure
+# 2. Providing a convention for the value of those keys
+# 3. Defining `attr_accessor`'s that are prefilled from an options hash given to `#initialize`
+# 4. Sharing instances to help GC
 
 require 'rep/version'
+
+# `Forwardable` is in the stdlib and allows ruby objects to delegate methods off to other objects. An example:
+#
+#     class A
+#       extend Forwardable
+#       delegate [:length, :first] => :@array
+#       def initialize(array = [])
+#         @array = array
+#       end
+#     end
+#
+#     A.new([1,2,3]).length # => 3
+#     A.new([1,2,3]).first  # => 1
+
 require 'forwardable'
+
+# `JSON::generate` and `JSON::decode` are much safer to use than `Object#to_json`.
+
 require 'json'
 
 module Rep
+
+  # All classes that `include Rep` are extended with `Forwardable`,
+  # given an alias `forward` for `delegate` (because it sounds better),
+  # an alias `fields` for `json_fields` unless it's already defined (because it sounds better),
+  # include `HashieSupport` which wraps all return values from `#to_hash` with `Hashie::Mash`,
+  # and setup an empty `parse_opts` if there is not already one which is used for shared instances so that options can be parsed without going through `#initialize`.
+
   def self.included(klass)
     klass.extend Forwardable
     klass.extend ClassMethods
@@ -27,18 +51,26 @@ module Rep
       end
 
       unless defined?(parse_opts)
-        def parse_opts(opts = {})
-          # NOOP
-        end
+        def parse_opts(opts = {}); end
       end
     }
   end
+
+  # Since a goal is to be able to share instances, we need an easy way to reset a
+  # shared instance back to factory defaults. If you memoize any methods that are
+  # not declared as json fields, then overried this method and super.
 
   def reset_for_json!
     self.class.all_json_methods.each do |method_name|
       instance_variable_set(:"@#{method_name}", nil)
     end
   end
+
+  # All the work of generating a hash from an instance is packaged up in one method. Since
+  # fields can be aliases themselves in the format { :json_key_name => :method_name }, there
+  # is some fancy logic to determine the `field_name` and `method_name` variables.
+  #
+  # { :one => :foo }.to_a => [[:one, :foo]]
 
   def to_hash(name = :default)
     if fields = self.class.json_fields(name)
@@ -57,11 +89,22 @@ module Rep
     end
   end
 
+  # `#to_json` is only for compatability. Use `JSON::generate` instead of delegating to the hash object's `#to_json` because I don't trust it.
+
   def to_json
     JSON.generate(to_hash)
   end
 
   module ClassMethods
+
+    # Defines an attr_accessor with a default value. The default for default is nil. Example:
+    #
+    #     class A
+    #       register_accessor :name => "No Name"
+    #     end
+    #
+    #     A.new.name # => "No Name"
+
     def register_accessor(acc)
       name, default = acc.is_a?(Hash) ? acc.to_a.first : [acc, nil]
       attr_accessor name
@@ -73,16 +116,29 @@ module Rep
       end
     end
 
+    # Defines an `#initialize` method that accepts a Hash argument and copies some keys out into `attr_accessors`.
+    # If your class already has an `#iniatialize` method then this will overwrite it. `#initialize_with` does not have to be used
+    # to use any other parts of Rep.
+
     def initialize_with(*args)
       @initializiation_args = args
+
+      # Remember what args we normally initialize with so we can refer to them when building shared instances.
 
       define_singleton_method :initializiation_args do
         @initializiation_args
       end
 
+      # Create an `attr_accessor` for each one. Defaults can be provided using the Hash version { :arg => :default_value }
+
       args.each { |a| register_accessor(a) }
 
+      # `#initialize` only parses the options
+
       define_method(:initialize) { |opts = {}| parse_opts(opts) }
+
+      # `#parse_opts` is responsable for getting the `attr_accessor` values prefilled. Since defaults can be specified, it
+      # must negotiate Hashes and use the first key of the hash for the `attr_accessor`'s name.
 
       define_method :parse_opts do |opts|
         @presidential_options = opts
@@ -92,6 +148,32 @@ module Rep
         end
       end
     end
+
+    # `#json_fields` setups up some class instance variables to remember sets of top level keys for json structures. Example:
+    #
+    #     class A
+    #       json_fields [:one, :two, :three] => :default
+    #     end
+    #
+    #     A.json_fields(:default) # => [:one, :two, :three]
+    #
+    # There is a general assumption that each top level key's value is provided by a method of the same name on an instance
+    # of the class. If this is not true, a Hash syntax can be used to alias to a different method name. Example:
+    #
+    #     class A
+    #       json_fields [{ :one => :the_real_one_method }, :two, { :three => :some_other_three }] => :default
+    #     end
+    #
+    # Once can also set multiple sets of fields. Example:
+    #
+    #     class A
+    #       json_fields [:one, :two, :three] => :default
+    #       json_fields [:five, :two, :six] => :other
+    #     end
+    #
+    # And all fields are returned by calling `#json_fields` with no args. Example:
+    #
+    #     A.json_fields # => { :default => [:one, :two, :three], :other => [:five, :two, :six] }
 
     def json_fields(arg = nil)
       if arg.is_a?(Hash)
