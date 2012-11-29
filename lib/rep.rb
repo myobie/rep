ced75244a5381f6b38a883d690267aea9285ef11
myobie <nathan@myobie.com>
2012-11-29 14:57:23 -0800

    ActiveSupport is causing me issues.

--- lib/rep.rb
+++ lib/rep.rb
@@ -37,6 +37,17 @@ module Rep
     klass.extend Forwardable
     klass.extend ClassMethods
     klass.instance_eval {
+      # Translate for ActiveSupport, the jerk
+      if method(:delegate).arity == -1
+        class << self
+          def delegate(opts = {})
+            methods, object_name = opts.to_a.first
+            args = methods.concat([:to => object_name])
+            super(*args)
+          end
+        end
+      end
+
       class << self
         unless defined?(delegate)
           def delegate(opts = {})
@@ -54,15 +65,6 @@ module Rep
 
         if defined?(Hashie)
           include HashieSupport
-        end
-      end
-
-      # Translate for ActiveSupport, the jerk
-      if method(:delegate).arity == -1
-        def delegate(opts = {})
-          methods, object_name = opts.to_a.first
-          args = methods.concat([:to => object_name])
-          super(*args)
         end
       end
     }
